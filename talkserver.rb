46fec88f3a118ad1ee058cbc84b682ad6fcebe79
xraystyle <xraystyle@xraystyle.com>
2015-10-21 16:39:54 -0700

    Clean up unnecessary whitespace.

--- talkserver.rb
+++ talkserver.rb
@@ -9,28 +9,24 @@ ip_addr = `networksetup -getinfo #{mac_iface_name} | awk '/^IP address:/ {print 
 server = TCPServer.new(ip_addr, 8000)
 
 while (session = server.accept)
-	request = session.gets
-	session.print "HTTP/1.1 200/OK\rContent-type: text/html\r\n\r\n"
-	session.print "<html><head><title>Response from Ruby Web server</title></head>\r\n"
-	session.print "<body>request was:"
-	session.print request
-	session.print "</body></html>"
-
-	begin
-
-		if found = request.match(/%22(.*)%22/)
-			output = found.captures[0].gsub(/%20/,' ')
-			puts output
-			`say -v Daniel \"#{output}\"`
-		end
-
-	rescue => e
-		puts "Something broke. Debug info:"
-		puts "exception: #{e}"
-		puts "request: #{request}"
-		puts "found: #{found}"
-		puts "output: #{output}"
-	end
-
-	session.close
+  request = session.gets
+  session.print "HTTP/1.1 200/OK\rContent-type: text/html\r\n\r\n"
+  session.print "<html><head><title>Response from Ruby Web server</title></head>\r\n"
+  session.print "<body>request was:"
+  session.print request
+  session.print "</body></html>"
+  begin
+    if found = request.match(/%22(.*)%22/)
+      output = found.captures[0].gsub(/%20/,' ')
+      puts output
+      `say -v Daniel \"#{output}\"`
+    end
+  rescue => e
+    puts "Something broke. Debug info:"
+    puts "exception: #{e}"
+    puts "request: #{request}"
+    puts "found: #{found}"
+    puts "output: #{output}"
+  end
+  session.close
 end
